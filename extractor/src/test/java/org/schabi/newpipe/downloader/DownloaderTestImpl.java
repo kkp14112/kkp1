e6300fae4fe2681143af81e7f3243b1c840e00ea
TobiGr <tobigr@users.noreply.github.com>
2024-05-14 19:33:19 +0200

    [Unit testing] Update user agent to Firefox from 91 to 113

--- extractor/src/test/java/org/schabi/newpipe/downloader/DownloaderTestImpl.java
+++ extractor/src/test/java/org/schabi/newpipe/downloader/DownloaderTestImpl.java
@@ -19,7 +19,7 @@ import okhttp3.ResponseBody;
 
 public final class DownloaderTestImpl extends Downloader {
     private static final String USER_AGENT
-            = "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0";
+            = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0";
     private static DownloaderTestImpl instance;
     private final OkHttpClient client;
 
